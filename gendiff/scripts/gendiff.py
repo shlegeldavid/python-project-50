d3b5e8c113a0a9b7399efc21a98ea648a42a488c
shlegeldavid <shlegeldavid@gmail.com>
2024-09-02 19:49:11 +0300

    Added format for gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,7 @@ def main():
 	)
 	parser.add_argument('first_file')
 	parser.add_argument('second_file')
+	parser.add_argument('-f', '--format', help = 'set format of output')
 	parser.parse_args(['-h'])
 
 
